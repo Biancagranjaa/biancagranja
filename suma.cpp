16da6c4defde09ee8a3154fb36d9310d418a1f1e
el profe <educayseft@gamil.com>
2022-09-14 13:58:32 -0500

    version2

--- suma.cpp
+++ suma.cpp
@@ -3,18 +3,10 @@ using namespace std;
 int main ()
 {
 	float A,B,C;
-	cin>>A>>B;
+	cout<<"Ingrese 2 numero A B :";
 	C=A+B;
+	cout<<"El resultado es:";
 	cout<<C;
 	return 0;
 }
- int main()
-{
-	float A,B,C;
-	cout<<"Ingrese 2 numeros A B :";
-	cin>>A>>B;
-	C=A+B;
-	cout<<"El resultado es :";
-	cout<<C;
-	return 0;
-}
+ 
