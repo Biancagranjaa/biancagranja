89d2480c3bd313c43733e7ccae2c6465d6ffc845
el profe <educayseft@gamil.com>
2022-09-14 13:47:46 -0500

    calculadora1

--- sumq.cpp
+++ sumq.cpp
@@ -1,14 +1,6 @@
 #include<iostream>
 using namespace std;
-int main ()
-{
-	float A,B,C;
-	cin>>A>>B;
-	C=A+B;
-	cout<<C;
-	return 0;
-}
- int main()
+int main()
 {
 	float A,B,C;
 	cout<<"Ingrese 2 numeros A B :";
